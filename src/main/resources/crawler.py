e6792f95f377af63dbf9701d6531a4144fac83ad
abstraction <2830391724@qq.com>
2023-04-24 14:44:34 +0800

    + 更新爬虫

--- src/main/resources/crawler.py
+++ src/main/resources/crawler.py
@@ -1,5 +1,9 @@
 import requests
+import pandas as pd
+import os
+
 # 爬虫: 酷我音乐飙升榜 http://www.kuwo.cn/rankList
+# 自己手动添加则推荐使用mp3tag
 
 if __name__ == '__main__':
     headers = {'Accept': ',application/json, text/plain, */*',
@@ -13,14 +17,49 @@ if __name__ == '__main__':
     music_url = 'http://www.kuwo.cn/api/v1/www/music/playUrl?mid={}' \
                 '&type=music&httpsStatus=1&reqId=d4578181-e1dd-11ed-92a0-9d4901615d88 '
 
-    for page_number in range(1, 31):
-        res = requests.get(rank_list_url.format(page_number), headers=headers)
+    if not os.path.exists('album_pic'):
+        os.makedirs('album_pic')
+    if not os.path.exists('mp3'):
+        os.makedirs('mp3')
+
+    name_list = []
+    artist_list = []
+    album_list = []
+    release_date_list = []
+    music_url_list = []
+    album_pic_list = []
+    for page_number in range(1, 16):
+        res = requests.get(rank_list_url.format(page_number), headers=headers)  # 音乐各项信息
         music_list = res.json()['data']['musicList']
         for music in music_list:
+            mp3 = requests.get(music_url.format(music['rid']), headers=headers)  # 音乐实际地址
             try:
-                mp3 = requests.get(music_url.format(music['rid']), headers=headers)
-                print(music['albumpic'], music['name'], music['artist'], music['album'], music['releaseDate'],
-                      mp3.json()['data']['url'])
-            except:
-                print('收费歌曲', music['name'])
+                music_url_list.append(mp3.json()['data']['url'])
+                name_list.append(music['name'])
+                artist_list.append(music['artist'])
+                album_list.append(music['album'])
+                release_date_list.append(music['releaseDate'])
+                album_pic_list.append(music['albumpic'])
 
+                with open('mp3/' + music['name'] + '.mp3', 'wb') as f:
+                    res = requests.get(mp3.json()['data']['url'], headers=headers)
+                    f.write(res.content)
+                with open('album_pic/' + music['album'] + '.jpg', 'wb') as f:
+                    res = requests.get(music['albumpic'], headers=headers)
+                    f.write(res.content)
+
+                print('已完成下载:', music['name'])
+
+            except Exception as e:
+                print('付费歌曲:', music['name'])
+
+    dfData = {  # 用字典设置DataFrame所需数据
+        '音乐名': name_list,
+        '歌手': artist_list,
+        '专辑': album_list,
+        '发布时间': release_date_list,
+        '音乐下载地址': music_url_list,
+        '专辑封面': album_pic_list
+    }
+    df = pd.DataFrame(dfData)  # 创建DataFrame
+    df.to_excel('music.xlsx', index=False)
