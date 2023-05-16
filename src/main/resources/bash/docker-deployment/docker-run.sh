7e19f8397a9d0dd9f1462d85d72f5cd1cb7433c8
abstraction <2830391724@qq.com>
2023-05-16 21:24:21 +0800

    ↑ 提供docker部署方式

--- src/main/resources/bash/docker-deployment/docker-run.sh
+++ src/main/resources/bash/docker-deployment/docker-run.sh
@@ -1,2 +1,4 @@
 docker pull abstract1on/music-box-backend:1.0.0
 docker run -p 8080:8080 -v /home/music-box/backend:/home/music-box/backend --name music-box-backend-main -d music-box-backend
+# 或者这个
+docker run -p 8080:8080 -v /home/music-box/backend:/home/music-box/backend --name music-box-backend-main -d abstract1on/music-box-backend:1.0.0
