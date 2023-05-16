595b653cfaa744e08c2b84b30c2177a2d2d2c7bb
abstraction <2830391724@qq.com>
2023-05-17 01:26:23 +0800

    - 删除无效文件

--- src/main/resources/bash/docker-deployment/docker-build.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p /home/music-box/backend
-cd  /home/music-box/backend
-# 需要先传输文件(music-box-0.0.1-SNAPSHOT.jar与Dockerfile到/home/music-box/backend)
-docker build -t music-box-backend .
-# run
-docker run -p 8080:8080 -v /home/music-box/backend:/home/music-box/backend --name music-box-backend-main -d music-box-backend
